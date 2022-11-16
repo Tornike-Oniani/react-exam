88b6f551cb0e3b82cd41950e3320bc96fcf6bf46
Tornike Oniani <dragonforwar@gmail.com>
2022-11-16 21:53:51 +0400

    cleanup

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,10 +5,7 @@ import './header.style.scss';
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo-box">
-        <img src="/img/logo.png" alt="Logo" className="logo" />
-      </div>
-      <h1 className="title">Userly</h1>
+      <h1 className="title">React Midterm</h1>
     </div>
   );
 };

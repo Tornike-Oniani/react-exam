d65539cc8fbeac7281d3a1a65b3b8cb02dea06c3
Tornike Oniani <dragonforwar@gmail.com>
2022-11-25 23:00:39 +0400

    removed unnecessary promises from service.js

--- src/api/service.js
+++ src/api/service.js
@@ -1,52 +1,30 @@
 import { baseUrl } from './base';
 
 export const getUsers = () => {
-  return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/users`, {
-      method: 'GET',
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        resolve(response);
-      });
-  });
+  return fetch(`${baseUrl}/users`, {
+    method: 'GET',
+  }).then((res) => res.json());
 };
 
 export const getFullUser = (id) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/users/${id}`, { method: 'GET' })
-      .then((res) => res.json())
-      .then((response) => {
-        resolve(response);
-      });
-  });
+  return fetch(`${baseUrl}/users/${id}`, { method: 'GET' }).then((res) =>
+    res.json()
+  );
 };
 
 export const getUserPosts = (id) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/posts/`, { method: 'GET' })
-      .then((res) => res.json())
-      .then((response) => {
-        const userPosts = response.filter((post) => post.userId == id);
-        resolve(userPosts);
-      });
-  });
+  return fetch(`${baseUrl}/posts/`, { method: 'GET' })
+    .then((res) => res.json())
+    .then((response) => {
+      const userPosts = response.filter((post) => post.userId == id);
+      return userPosts;
+    });
 };
 
 export const getFullPost = (id) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/posts/${id}`)
-      .then((res) => res.json())
-      .then((response) => {
-        resolve(response);
-      });
-  });
+  return fetch(`${baseUrl}/posts/${id}`).then((res) => res.json());
 };
 
 export const getPostComments = (id) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/posts/${id}/comments`)
-      .then((res) => res.json())
-      .then((response) => resolve(response));
-  });
+  return fetch(`${baseUrl}/posts/${id}/comments`).then((res) => res.json());
 };
